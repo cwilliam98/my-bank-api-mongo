945ce5089fd8837af49978ec5d01856cbbc48870
cwilliam98 <cristianwillian174@gmail.com>
2020-09-03 11:30:26 -0300

    Configurando heroku

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import accountsRouter from './routes/accountsRouter.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 const app = express();
-const params = dotenv.config();
-
 (async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${params.parsed.USERDB}:${params.parsed.USERDB}@cluster0.1v9ej.mongodb.net/myBank?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.1v9ej.mongodb.net/myBank?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -23,6 +21,6 @@ const params = dotenv.config();
 app.use(express.json());
 app.use('/account', accountsRouter);
 
-app.listen(params.parsed.PORT, () => {
-  console.log('App listen on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log('App listen on port' + process.env.PORT);
 });
